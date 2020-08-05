10658f69d182678a68686015b95903c06461daaa
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2020-08-05 22:12:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+     /* Navbar scroll */
+        $(function() {
+
+            var nav = $('.navbar'),
+                doc = $(document),
+                win = $(window);
+
+            win.scroll(function() {
+
+                if (doc.scrollTop() > 80) {
+                    nav.addClass('scrolled');
+                } else {
+                    nav.removeClass('scrolled');
+                }
+
+            });
+
+            win.scroll();
+
+        });
+
+        /* ***** Smooth Scrolling  ***** */
+        $(document).ready(function() {
+            $(".navbar a, #service a").on('click', function(event) {
+
+                if (this.hash !== "") {
+                    event.preventDefault();
+                    var hash = this.hash;
+
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 900, function() {
+
+                        window.location.hash = hash;
+                    });
+                }
+            });
+
+
+            /* ***** Scroll to Top ***** */
+            $(window).scroll(function() {
+                if ($(this).scrollTop() >= 300) {
+                    $('.to-top').fadeIn(200);
+                } else {
+                    $('.to-top').fadeOut(200);
+                }
+            });
+            $('.to-top').click(function() {
+                $('.body,html').animate({
+                    scrollTop: 0
+                }, 500);
+            });
+
+        })
